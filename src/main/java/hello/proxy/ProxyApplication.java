2b1891225e73d12a49a40a38d58b75ce8758a780
kkyu8925 <kkyu8925@gmail.com>
2022-12-02 22:49:39 +0900

    동적 프록시 기술

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,8 +1,11 @@
 package hello.proxy;
 
 import hello.proxy.config.v6_aop.AopConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import({AppV1Config.class, AppV2Config.class})
@@ -20,5 +23,10 @@ public class ProxyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProxyApplication.class, args);
+    }
+
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
     }
 }
